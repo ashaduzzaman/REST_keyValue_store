20320c8b5ac4b1f95bd06486ec38f9e31aa27fe8
zrshishir <zrshishir@gmail.com>
2019-12-30 21:36:32 +0600

    hh

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.6.6deb5
+ */
+
+/**
+ * Database `prescription_new_5`
+ */
+
+/* `prescription_new_5`.`test` */
+$test = array(
+  array('id' => '1','group' => 'Imaging','name' => 'MRI','created_at' => '2019-09-16 09:42:52','updated_at' => '2019-09-16 09:42:52'),
+  array('id' => '2','group' => 'Brain','name' => 'CT Scan','created_at' => '2019-09-16 09:43:06','updated_at' => '2019-09-16 09:43:06'),
+  array('id' => '3','group' => 'Blood','name' => 'CBC','created_at' => '2019-09-16 09:43:19','updated_at' => '2019-09-16 09:43:19'),
+  array('id' => '4','group' => 'Denggu','name' => 'NS1','created_at' => '2019-09-16 09:43:43','updated_at' => '2019-09-16 09:43:43')
+);
